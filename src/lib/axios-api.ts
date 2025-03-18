3515c07581801141da869fe37d86b1d8ffb66e56
GustavoeDev <gustavoe.dev@gmail.com>
2025-03-17 22:38:05 -0300

    feat: add axios for API requests and create axiosAPI utility function

---
+++ src/lib/axios-api.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import { APIError } from "@/types/api";
+import { cookies } from "next/headers";
+import axios, { AxiosError } from "axios";
+
+interface APIProps {
+  endpoint: string;
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  data?: object;
+  withAuth?: boolean;
+  withAttachment?: boolean;
+}
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL + "/api/v1";
+
+export const axiosAPI = async <TypeResponse>({
+  endpoint,
+  method = "GET",
+  data,
+  withAuth = true,
+  withAttachment = false,
+}: APIProps) => {
+  const instance = axios.create({
+    baseURL: BASE_URL,
+  });
+
+  if (withAuth) {
+    const sessionAuth = (await cookies()).get(
+      process.env.NEXT_PUBLIC_AUTH_KEY as string
+    );
+
+    if (sessionAuth?.value) {
+      instance.defaults.headers.common[
+        "Authorization"
+      ] = `Bearer ${sessionAuth.value}`;
+    }
+  }
+
+  if (withAttachment) {
+    instance.defaults.headers.common["Content-Type"] = "multipart/form-data";
+  }
+
+  try {
+    const request = await instance<TypeResponse>(endpoint, {
+      method,
+      params: method === "GET" && data,
+      data: method !== "GET" && data,
+    });
+
+    return {
+      data: request.data,
+    };
+  } catch (error) {
+    const e = error as AxiosError<APIError>;
+    return {
+      error: {
+        message: e.response?.data.detail ?? "Ocorreu um erro inesperado",
+      },
+    };
+  }
+};
